0e287e9405ea1ef92534f6643052abbc6c131508
Neil <neil@Carries-MacBook-Air.local>
2019-05-08 22:57:38 -0400

    heroku still not working. test 10?

--- server.js
+++ server.js
@@ -124,8 +124,9 @@ app.use('/', placesRoutes)
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
+  
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')) // relative path
   })
 }
 
